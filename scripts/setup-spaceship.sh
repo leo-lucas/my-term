9e36aa7eaed1e3f43977ac9c9dca0bac4487e3ec
Leonardo Lucas dos Santos <leolucassang@gmail.com>
2026-01-01 12:20:27 -0300

    Install Spaceship in Oh My Zsh themes root

--- scripts/setup-spaceship.sh
+++ scripts/setup-spaceship.sh
@@ -13,11 +13,10 @@ if [[ ! -d "${zsh_dir}" ]]; then
   exit 1
 fi
 
-zsh_custom="${ZSH_CUSTOM:-${zsh_dir}/custom}"
-echo "Usando diretório custom do Oh My Zsh: ${zsh_custom}"
-spaceship_dir="${zsh_custom}/themes/spaceship-prompt"
+themes_dir="${zsh_dir}/themes"
+spaceship_dir="${themes_dir}/spaceship-prompt"
 
-mkdir -p "${zsh_custom}/themes"
+mkdir -p "${themes_dir}"
 
 if [[ ! -d "${spaceship_dir}" ]]; then
   git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "${spaceship_dir}"
@@ -26,7 +25,7 @@ else
   echo "Tema Spaceship já está instalado."
 fi
 
-ln -sf "${spaceship_dir}/spaceship.zsh-theme" "${zsh_custom}/themes/spaceship.zsh-theme"
+ln -sf "${spaceship_dir}/spaceship.zsh-theme" "${themes_dir}/spaceship.zsh-theme"
 
 os_name="$(uname -s)"
 zshrc_path="${HOME}/.zshrc"
